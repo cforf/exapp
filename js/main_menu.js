a5ea4c3c43bbb02e23a69c196c6ab0c37896440a
cforf <cforfwork@gmail.com>
2017-05-05 15:03:31 +0300

    main menu handling 2

--- js/main_menu.js
+++ js/main_menu.js
@@ -3,29 +3,42 @@ var menu = (function () {
 
     var menuItems = document.querySelectorAll('#content div');
 
-    console.log('-begin work-');
-
     function menuOver(e) {
-
-        var menuItem = e.target;
-        console.log(menuItem.tagName);
-        console.log("You over menu item!");
-        console.log(menuItem.getAttribute('id'));
-        console.log(menuItem.getElementsByTagName('img')[0]);
-
-        menuItem.getElementsByTagName('img')[0].style.display = 'inline';
+        e.target.getElementsByTagName('img')[0].style.display = 'inline';
     }
 
     function menuOut(e) {
-        console.log("You out of menu item!");
         e.target.getElementsByTagName('img')[0].style.display = 'none';
     }
 
-    for (var i = 0; i < menuItems.length; i++) {
-        menuItems[i].addEventListener('mouseenter', menuOver, false);
-        menuItems[i].addEventListener('mouseleave', menuOut, false);
+    function menuClick(e) {
+        var item;
+        if (e.target.tagName === 'DIV') {
+            //console.log('- click -' + );
+            item = e.target.getAttribute('id');
+        } else {
+            //console.log(e.target.parentNode.getAttribute('id'));
+            item = e.target.parentNode.getAttribute('id');
+        }
+
+        console.log(item);
+
+        switch (item) {
+            case 'sol1':
+                location = "first.html";
+                break;
+            case 'sol2':
+                location = "second.html";
+                break;
+        }
+
     }
 
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('mouseenter', menuOver);
+        menuItems[i].addEventListener('mouseleave', menuOut);
+        menuItems[i].addEventListener('click', menuClick, false);
+    }
 
 
 })();
