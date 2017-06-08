6853342e2e2865129b5b51ef705fd359bac099be
cforf <cforfwork@gmail.com>
2017-06-08 12:56:12 +0300

    example1: edit 4 - fields

--- js/example1.js
+++ js/example1.js
@@ -1,10 +1,47 @@
 var example1 = (function () {
 
+    // Firefox 1.0+
+    //var isFirefox = typeof InstallTrigger !== 'undefined';
+
     var dateField = document.getElementById('date');
 
+    var numField = document.getElementById('nums');
+
+
+
     console.log(dateField.getAttribute('value'));
 
-    dateField.setAttribute('value', '2017-06-07');
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    //today = mm + '-' + dd + '-' + yyyy;
+
+    /*if (isFirefox) {
+        today = dd + '.' + mm + '.' + yyyy;
+    } else {
+        today = yyyy + '-' + mm + '-' + dd;
+    }*/
+
+    today = dd + '.' + mm + '.' + yyyy;
+
+    dateField.setAttribute('value', today);
 
     console.log(dateField.getAttribute('value'));
+
+
+
+    numField.addEventListener('focus', function () {
+        numField.style.fontSize = "2em";
+    });
 })();
