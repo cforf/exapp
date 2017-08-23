71ca38f9ff775a65cce941c8b06893775bba0366
cforf <cforfwork@gmail.com>
2017-08-23 19:26:26 +0300

    example3: edit 1

---
+++ example3/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name example3App
+ * @description
+ * # example3App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('example3App', []);
