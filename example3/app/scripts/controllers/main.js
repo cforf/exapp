71ba4ef25cbd5ecf1d708c259dcff014438452b3
cforf <cforfwork@gmail.com>
2017-08-29 23:56:42 +0300

    example3: edit 3

--- example3/app/scripts/controllers/main.js
+++ example3/app/scripts/controllers/main.js
@@ -10,8 +10,8 @@
 
 var data = [2525, 2626, 2727, 2828, 2929, 3030, 3131, 3232];
 
-angular.module('example3App').controller('MainCtrl', function ($scope) {
+angular.module('example3App').controller('MainCtrl', function($scope) {
 
-  $scope.ajaxData = data;
+    $scope.ajaxData = data;
 
 });
