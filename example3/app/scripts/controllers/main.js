28f0594f756b79a9af2339bdea0f3257a8c3b387
cforf <cforfwork@gmail.com>
2017-08-30 15:53:27 +0300

    example3: end of handling btn submit

--- example3/app/scripts/controllers/main.js
+++ example3/app/scripts/controllers/main.js
@@ -15,32 +15,55 @@ angular.module('example3App').controller('MainCtrl', function($scope) {
     $scope.ajaxData = data;
 
     $scope.mouseEnter = function() {
-        console.log("mouse over");
+        console.log('mouse over');
 
         $scope.btnStyle = {
-            "margin-top": "-5px",
-            "cursor": "pointer",
-            "box-shadow": "0px 8px 2px rgba(0,0,0,.25)"
+            'margin-top': '-5px',
+            'cursor': 'pointer',
+            'box-shadow': '0px 8px 2px rgba(0,0,0,.25)'
         };
 
         $scope.sectionStyle = {
-            "margin-top": "35px"
+            'margin-top': '35px'
+        };
+    };
+
+    $scope.mouseDown = function() {
+        console.log('mouse down');
+
+        $scope.btnStyle = {
+            'margin-top': '0px',
+            '-webkit - box - shadow': 'none',
+            'box-shadow': 'none'
+        };
+
+        $scope.sectionStyle = {
+            'margin-top': '30px'
+        };
+    };
+
+    $scope.mouseLeave = function() {
+        console.log('mouse leave');
+
+        $scope.btnStyle = {
+            'margin-top': '0px',
+            '-webkit - box - shadow': 'none',
+            'box-shadow': 'none'
+        };
+
+        $scope.sectionStyle = {
+            'margin-top': '30px'
         };
     };
 
     $scope.btnStyle = {
-        "-webkit - box - shadow": "none",
-        "box - shadow": "none",
-        "color": "#363636",
-        "background - color": "#ff7800",
-        "border": "2 px solid #363636",
-        "border-radius": "8px",
-        "font-family": "dejavu",
-        "font-size": "1.8em"
+        '-webkit - box - shadow': 'none',
+        'box - shadow': 'none',
+        'margin-top': '0px'
     };
 
     $scope.sectionStyle = {
-        "margin-top": "30px"
+        'margin-top': '30px'
     };
 
 
