6b511d5b8aa1a5758e0d373b5813242a047b7d25
cforf <cforfwork@gmail.com>
2017-09-05 13:24:03 +0300

    example3: validation form

--- example3/app/scripts/controllers/form.js
+++ example3/app/scripts/controllers/form.js
@@ -9,15 +9,28 @@
  */
 
 
-angular.module('example3App').controller('formCtrl', function($scope) {
+angular.module('example3App').controller('formCtrl', function($scope, $filter) {
+    var stringDate = $filter('date')(new Date(), 'yyyy-MM-dd');
+
     $scope.dataForm = [];
 
-    $scope.date = new Date();
+    $scope.dateData = new Date(stringDate);
+
+    // $scope.date = $filter('date')($scope.date, 'yyyy-MM-dd');
+    // var today = $filter('date')(new Date(), 'yyyy-MM-dd');
+    // $scope.date = $filter('date')(new Date(), 'yyyy-MM-dd'); //today;
     $scope.numData = null;
 
     $scope.addDataForm = function() {
         console.log('submit data from form ');
-        $scope.dataForm.push($scope.date, $scope.numData);
+        $scope.dataForm.push($filter('date')($scope.dateData, 'yyyy-MM-dd'), $scope.numData);
         console.log($scope.dataForm);
+        //clear fields-->
+    };
+
+    $scope.check = function() {
+        // console.log($filter('date')($scope.date, 'yyyy-MM-dd'));
+        console.log('stringDate= ' + stringDate);
+        console.log('$scope.date = ' + $scope.dateData);
     };
 });
